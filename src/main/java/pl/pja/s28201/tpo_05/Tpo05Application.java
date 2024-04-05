c9d8f3cad266a5d5990c5013f11074381dae88a6
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-04-05 10:55:15 +0200

    Initial commit

---
+++ src/main/java/pl/pja/s28201/tpo_05/Tpo05Application.java
@@ -0,0 +1,13 @@
+package pl.pja.s28201.tpo_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpo05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tpo05Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pja/s28201/tpo_05/Tpo05ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pja.s28201.tpo_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpo05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
